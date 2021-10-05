de4ebc5cbacf5e540b8cdc5779e02b4ec3f25166
Karolis_Medeksa <medeksa.karolis@gmail.com>
2021-10-05 15:21:18 +0200

    Implement I/O operations

--- classifier.py
+++ classifier.py
@@ -1,1 +1,46 @@
-print("Hi")
+import pandas as pd
+
+DATA_INPUT_FILENAME = 'input.txt'
+DATA_OUTPUT_FILENAME = 'classified.xlsx'
+
+def read_DataFrame_from_file():
+    return pd.read_csv(DATA_INPUT_FILENAME, delimiter='\t')
+
+
+def write_DataFrame_to_excel(df: pd.DataFrame):
+    sheet_name = 'Classified'
+
+    with pd.ExcelWriter(DATA_OUTPUT_FILENAME, engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+        
+        worksheet = writer.sheets[sheet_name]
+        # format all data as a table
+        worksheet.add_table(0, 0, df.shape[0], df.shape[1]-1, {
+            'columns': [{'header': col_name} for col_name in df.columns],
+            'style': 'Table Style Medium 5'
+        })
+        # Widen the address column
+        worksheet.set_column('C:C', 70)
+
+        # Add formatting - red for negative, green - for positive qualification
+        redFormat = writer.book.add_format({'bg_color': '#FF0000', 'font_color': '#000000'})
+        greenFormat = writer.book.add_format({'bg_color': '#00B050', 'font_color': '#000000'})
+        worksheet.conditional_format(0, 0, df.shape[0], df.shape[1]-1, {'type': 'formula',
+                                          'criteria': '=$E1=0',
+                                          'format': redFormat})
+        worksheet.conditional_format(0, 0, df.shape[0], df.shape[1]-1, {'type': 'formula',
+                                          'criteria': '=$E1=1',
+                                          'format': greenFormat})
+
+
+def classify_address(dataFrame: pd.DataFrame):
+    dataFrame['complete'] = 0
+    return dataFrame
+
+
+def init():
+    classified = classify_address(read_DataFrame_from_file())
+    write_DataFrame_to_excel(classified)
+
+
+init()
